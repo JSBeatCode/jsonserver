258b1b7a649e571a199f3409c52971a694bbb444
jsdno0 <csdnum0@gmail.com>
2021-03-04 21:13:59 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Json Server is Running</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
